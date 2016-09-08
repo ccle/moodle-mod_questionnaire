6d90887d813b5fb305201e93e1f912efb60e4ef2
Anant <mahajananant@gmail.com>
2016-09-08 19:31:24 +0400

    CCLE-6066 - Upgrade mod_questionnaire to 2016020203

--- questionnaire.class.php
+++ questionnaire.class.php
@@ -2688,7 +2688,9 @@ class questionnaire {
             $config = get_config('questionnaire', 'downloadoptions');
         }
         $options = empty($config) ? array() : explode(',', $config);
-
+        // START UCLA MOD: CCLE-2596 - Add UCLA UID to Questionnaire download all responses option.
+        array_splice($options, 8, 0, 'idnumber');
+        // END UCLA MOD: CCLE-2596.
         $positioned = [];
         $user = new stdClass();
         foreach ($this->user_fields() as $userfield) {
@@ -2773,6 +2775,13 @@ class questionnaire {
         if (in_array('fullname', $options)) {
             array_push($positioned, $fullname);
         }
+        // START UCLA MOD: CCLE-2596 - Add UCLA UID to Questionnaire download all responses option.
+        if (in_array('idnumber', $options)) {
+            if ($user = $DB->get_record('user', array('id' => $user->id))) {
+                array_push($positioned, $user->idnumber);
+            }
+        }
+        // END UCLA MOD: CCLE-2596.
         if (in_array('username', $options)) {
             array_push($positioned, $username);
         }
@@ -2834,6 +2843,12 @@ class questionnaire {
             '1',    // 9: date -> string
             '0'     // 10: numeric -> number.
         );
+        // START UCLA MOD: CCLE-2596 - Add UCLA UID to Questionnaire download all responses option.
+        ++$nbinfocols;
+        // Place idnumber fullname, which is position 8.
+        array_splice($columns, 8, 0, get_string('idnumber'));
+        array_splice($types, 8, 0, 1);
+        // END UCLA MOD: CCLE-2596.
 
         if (!$survey = $DB->get_record('questionnaire_survey', array('id' => $this->survey->id))) {
             print_error ('surveynotexists', 'questionnaire');
@@ -3129,7 +3144,7 @@ class questionnaire {
         $numquestion = 0;
         $out = '';
         $oldkey = 0;
-
+        
         for ($i = $nbinfocols; $i < $numrespcols; $i++) {
             $sep = '';
             $thisoutput = current($output[0][$i]);
